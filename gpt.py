0d7b4fb81bcac8eb443ab5fabcfdbf6ade4d5163
lewardo <segfault.ninja@gmail.com>
2023-11-12 11:48:48 +0000

    conversations require memory tho idiot

--- gpt.py
+++ gpt.py
@@ -5,15 +5,19 @@ client = OpenAI(
     api_key=config("OPENAI_API_KEY")
 )
 
+messages = [
+    {"role": "system", "content": "You are a sarcastic assistant that answers questions reluctantly and in no more than one sentence, as well as subtly insulting the user, and never prompting for more input"},
+]
+
 def get_completion(text):
-    
+    messages.append({"role": "user", "content": text})
+
     completion = client.chat.completions.create(
         model = "gpt-3.5-turbo",
-        messages = [
-            {"role": "system", "content": "You are a sarcastic assistant that answers questions reluctantly and in no more than one sentence, as well as subtly insulting the user, and never prompting for more input"},
-            {"role": "user", "content": text}
-        ]
+        messages = messages
     )
+
+    messages.append({"role": "system", "content": completion.choices[0].message.content})
 
     return completion.choices[0].message.content
 			
